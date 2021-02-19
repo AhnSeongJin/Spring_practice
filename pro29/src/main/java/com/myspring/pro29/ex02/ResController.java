bd66ae57699355f5d6bf30aaceb12b3165922cec
seongjin <sokc625@gmail.com>
2021-02-19 10:56:36 +0900

    29장 REST

---
+++ pro29/src/main/java/com/myspring/pro29/ex02/ResController.java
@@ -0,0 +1,34 @@
+package com.myspring.pro29.ex02;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResController {
+	@RequestMapping(value = "/res1")
+	@ResponseBody
+	public Map<String, Object> res1() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", "hong");
+		map.put("name", "홍길동");
+		return map;
+	}
+	
+	
+	@RequestMapping(value = "/res2")
+	public ModelAndView res2() {
+		return new ModelAndView("home");
+	}
+	
+	// @ResponseBody 사용하면 선택적으로 rest 기능 사용 가능
+	// 붙이면 json 형식, 아니면 원래대로 ModelAndView 로 jsp 
+	
+}
